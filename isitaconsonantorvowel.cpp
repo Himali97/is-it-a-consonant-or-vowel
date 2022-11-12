ac2dbb26d905a5b87edd91f39234896a1c6e95aa
Himali97 <117569161+Himali97@users.noreply.github.com>
2022-11-12 18:16:12 +0530

    Add files via upload

---
+++ isitaconsonantorvowel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    char c,a,e,i,o,u,A,E,I,O,U;
+    cin>>c;
+if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+    cout<<"Vowel"<<endl;
+    else
+    cout<<"Consonant"<<endl;    
+
+	return 0;
+}
